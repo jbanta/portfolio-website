928bfb3fcdb3d217ffbd088468bd3917b1859bf2
Johann Banta <johannbanta@gmail.com>
2018-11-27 19:28:49 -0800

    added mobile thumbnails for projects

---
+++ js/workThumbnailSwitch.js
@@ -0,0 +1,6 @@
+if (window.innerWidth <=820) {
+    $("#banner-y3di").html("<img class='proj-banner' src='assets/img/banner-y3di-mobile.png' alt='You3Dit Landing Page Redesign'>");
+    $("#banner-dfam").html("<img class='proj-banner' src='assets/img/banner-dfam-mobile.png' alt='DfAM Discussion Cards & Landing Page'>");
+    $("#banner-dailyui").html("<img class='proj-banner' src='assets/img/banner-dailyui-mobile.png' alt='Daily UI Challeng'>");
+    $(".expand-overlay").css("opacity", "0");
+}
