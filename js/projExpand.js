ebc56c33079e81a9402e6ed5db58dbaf574d3514
Johann Banta <johannbanta@gmail.com>
2018-11-28 14:16:03 -0800

    fixed overlay bug when loading page for first time

--- js/projExpand.js
+++ js/projExpand.js
@@ -3,11 +3,15 @@
 var curproj;
 var curbtn;
 var tophidebtn;
-if (window.innerWidth <=820) {
-  $(".expand-overlay").css({'height':($(".proj-banner").height()*1.72 +'px')});
-} else {
-  $(".expand-overlay").css({'height':($(".proj-banner").height() +'px')});
-}
+
+// setTimeout(
+//   function() {
+//     if (window.innerWidth <=820) {
+      $(".expand-overlay").css({'height':($(".proj-banner").height()*1.72 +'px')});
+    } else {
+      $(".expand-overlay").css({'height':($(".proj-banner").height() +'px')});
+    }
+  // }, 3000);
 $( ".expand-overlay").click(function() {
   curproj = "#proj-" + $( this ).attr('id');
   curbtn = "#btn-" + $( this ).attr('id');
