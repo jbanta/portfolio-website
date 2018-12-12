dab5c5823d59901040ed080d0f151e68d7238144
Johann Banta <johannbanta@gmail.com>
2018-12-11 20:23:32 -0800

    removed title loop for now and switched to only UI/UX Design

--- js/titleloop.js
+++ js/titleloop.js
@@ -1,11 +1,11 @@
-setInterval(loop, 1500);
-
-var i = 0
-var titles = ["UI / UX Design", "Product Design", "Branding Design", "Front-End Development"]
-
-function loop( ) {
-  i += 1;
-  $("#subtitle-hero").css("display", "none");
-  $('#subtitle-hero').html(titles[i % titles.length ]);
-  $('#subtitle-hero').fadeIn(100);
-}
+// setInterval(loop, 1500);
+//
+// var i = 0
+// var titles = ["UI / UX Design", "Product Design", "Branding Design", "Front-End Development"]
+//
+// function loop( ) {
+//   i += 1;
+//   $("#subtitle-hero").css("display", "none");
+//   $('#subtitle-hero').html(titles[i % titles.length ]);
+//   $('#subtitle-hero').fadeIn(100);
+// }
