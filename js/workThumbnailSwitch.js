f61806b9e52fe95ab822b45e6fe5dd06bf0e47aa
Johann Banta <johannbanta@gmail.com>
2018-12-18 19:04:04 -0800

    phase 2 start, homepage proj cards

--- js/workThumbnailSwitch.js
+++
@@ -1,6 +0,0 @@
-if (window.innerWidth <=820) {
-    $("#banner-y3di").html("<img class='proj-banner' src='assets/img/banner-y3di-mobile.png' alt='You3Dit Landing Page Redesign'>");
-    $("#banner-dfam").html("<img class='proj-banner' src='assets/img/banner-dfam-mobile.png' alt='DfAM Discussion Cards & Landing Page'>");
-    $("#banner-dailyui").html("<img class='proj-banner' src='assets/img/banner-dailyui-mobile.png' alt='Daily UI Challeng'>");
-    // $(".expand-overlay").css("opacity", "0");
-}
