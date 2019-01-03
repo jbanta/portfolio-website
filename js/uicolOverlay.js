f736a1bb39a5b37e4a453f3082e2114bf2bd1956
Johann Banta <johannbanta@gmail.com>
2019-01-03 12:35:30 -0800

    added dfam and ui collection page

---
+++ js/uicolOverlay.js
@@ -0,0 +1,19 @@
+$(window).load(function() {
+  // Open Overlay
+  $(".uicol-thumbnail").click(function() {
+      var itemNum = this.id.split('-')[1];
+      $('#overlay-' + itemNum).fadeIn();
+      $('body').css("overflow", "hidden");
+  });
+
+  //Close Overlay
+  $(".uicol-close-x").click(function() {
+      $('.uicol-overlay-container').fadeOut();
+      $('body').css("overflow-y", "scroll");
+  });
+
+  //Opens Overlay if specific item was linked
+  var itemNum = window.location.hash.substring(1).split('-')[0];
+  $('#overlay-' + itemNum).fadeIn();
+  $('body').css("overflow", "hidden");
+});
